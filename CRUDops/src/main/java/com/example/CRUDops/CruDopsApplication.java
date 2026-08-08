c8406a18d062a756c3063541fbe5f993b71d4e3c
Nikhil79922 <ns94301918@gmail.com>
2026-08-08 16:49:24 +0530

    Added the spring boot values and configurationProperties changes

---
+++ CRUDops/src/main/java/com/example/CRUDops/CruDopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDopsApplication.class, args);
+	}
+
+}
---
+++ CRUDops/src/test/java/com/example/CRUDops/CruDopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
